bf649c276779dfa83c4f6aafbfad608442ae026e
Alex Hadi <alexhisb@gmail.com>
2019-09-26 18:18:54 -0700

    More updates to the pom.xml file.

--- src/synthesizer/GoogleCloudSynthesizer.java
+++ src/synthesizer/GoogleCloudSynthesizer.java
@@ -1,33 +1,24 @@
 package synthesizer;
 
 import com.google.cloud.texttospeech.v1.*;
-import com.google.common.hash.Hashing;
 import com.google.protobuf.ByteString;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 
 public class GoogleCloudSynthesizer {
     public static void createPhraseAudioFiles() {
-        ArrayList<String> allPhrases = new ArrayList<>();
-        for (Phrase phrase : Phrase.values()) {
-            allPhrases.add(phrase.getPhraseValue());
-        }
-
         try (TextToSpeechClient textToSpeechClient = TextToSpeechClient.create()) {
-            for (String phraseValue : allPhrases) {
-                String phraseSha256Hash = Hashing.sha256().hashString(phraseValue, StandardCharsets.UTF_8).toString();
-                File phraseFile = new File("phrases/" + phraseSha256Hash + ".mp3");
+            for (Phrase phrase : Phrase.values()) {
+                File phraseFile = phrase.getPhraseAudioFile();
                 if (phraseFile.exists()) {
                     continue;
                 }
 
                 SynthesisInput input = SynthesisInput.newBuilder()
-                        .setText(phraseValue)
+                        .setText(phrase.getPhraseValue())
                         .build();
 
                 VoiceSelectionParams voice = VoiceSelectionParams.newBuilder()
