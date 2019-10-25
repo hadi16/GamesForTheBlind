f8e2bb32df4b06068ad13729eec1eeed80371374
Alex Hadi <alexhisb@gmail.com>
2019-10-25 11:26:43 -0700

    Improved audio feedback in the game. Added logic to delete old audio files.

--- src/synthesizer/AudioFileBuilder.java
+++ src/synthesizer/AudioFileBuilder.java
@@ -7,15 +7,33 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class GoogleCloudRetriever {
-    public static void createPhraseAudioFiles() {
+public class AudioFileBuilder {
+    private final File phraseDirectory;
+
+    public AudioFileBuilder() {
+        this.phraseDirectory = new File(Phrase.PHRASE_FILES_DIRECTORY.toString());
+        if (!this.phraseDirectory.exists()) {
+            this.phraseDirectory.mkdirs();
+        }
+    }
+
+    public void deleteOldPhraseAudioFiles() {
+        List<File> audioFiles = Arrays.asList(this.phraseDirectory.listFiles());
+        for (Phrase phrase : Phrase.values()) {
+            audioFiles.remove(phrase.getPhraseAudioFile());
+        }
+
+        for (File oldPhraseAudioFile : audioFiles) {
+            oldPhraseAudioFile.delete();
+        }
+    }
+
+    public void createPhraseAudioFiles() {
         try (TextToSpeechClient textToSpeechClient = TextToSpeechClient.create()) {
-            File phraseDirectory = new File(Phrase.PHRASE_FILES_DIRECTORY.toString());
-            if (!phraseDirectory.exists()) {
-                phraseDirectory.mkdirs();
-            }
-
             for (Phrase phrase : Phrase.values()) {
                 File phraseFile = phrase.getPhraseAudioFile();
                 if (phraseFile.exists()) {
