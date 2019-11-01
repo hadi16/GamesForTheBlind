df2d2be34c695c55cd9a5610e9a793062c047cf5
Alex Hadi <alexhisb@gmail.com>
2019-11-01 10:42:41 -0700

    The audio player doesn't pause any more.

--- src/gamesforblind/synthesizer/AudioPlayer.java
+++ src/gamesforblind/synthesizer/AudioPlayer.java
@@ -5,20 +5,28 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Optional;
 
 /**
  * Audio player for the program. Run on a separate thread to prevent audio from blocking the main program thread.
  */
 public class AudioPlayer implements Runnable {
     /**
-     * The audio clip that is currently playing.
+     * A Map between a Phrase in the game & an OPENED audio Clip. This prevents pauses in the
+     * game that previously occurred due to File IO taking a considerable amount of time.
      */
-    private final Clip clip;
+    private final HashMap<Phrase, Clip> phraseToClip;
 
     /**
      * When this is set to false, the audio player is terminated.
      */
     private boolean isActive = true;
+
+    /**
+     * The Clip that is currently playing or was the last Clip that played in the game.
+     */
+    private Clip activeClip;
 
     /**
      * The remaining phrases that need to be played in the game.
@@ -31,21 +39,47 @@ public class AudioPlayer implements Runnable {
      * @throws LineUnavailableException Thrown when the {@link Clip} in the audio player cannot be started.
      */
     public AudioPlayer() throws LineUnavailableException {
-        this.clip = AudioSystem.getClip();
+        // Initialize the active Clip to an empty Clip object (prevents the need to check for null)
+        this.activeClip = AudioSystem.getClip();
+
+        this.phraseToClip = this.getInitializedClipHashMap();
     }
 
     /**
-     * Resets the audio stream to the passed audio file.
-     *
-     * @param audioFile The audio file to initialize.
+     * Get the Clip associated with a particular Phrase. If an error occurs, return Optional.empty().
+     * @param phrase The Phrase to get the associated Clip from.
+     * @return An Optional containing the Clip if no error occurred (otherwise, empty()).
      */
-    private void resetAudioStream(File audioFile) {
+    private Optional<Clip> getPhraseClip(Phrase phrase) {
         try {
-            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(audioFile.getAbsoluteFile());
-            this.clip.open(audioInputStream);
-        } catch (IOException | UnsupportedAudioFileException | LineUnavailableException e) {
+            Clip phraseClip = AudioSystem.getClip();
+
+            File phraseAudioFile = phrase.getPhraseAudioFile().getAbsoluteFile();
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(phraseAudioFile);
+
+            // The Clip should be opened so that it can be immediately played.
+            phraseClip.open(audioInputStream);
+
+            return Optional.of(phraseClip);
+        } catch (IOException | LineUnavailableException | UnsupportedAudioFileException e) {
             e.printStackTrace();
         }
+
+        return Optional.empty();
+    }
+
+    /**
+     * Loops over all of the stored Phrases and gets an opened Clip to play with each Phrase.
+     * @return A HashMap between a Phrase in the game and an opened audio Clip.
+     */
+    private HashMap<Phrase, Clip> getInitializedClipHashMap() {
+        HashMap<Phrase, Clip> phraseToClip = new HashMap<>();
+        for (Phrase phrase : Phrase.values()) {
+            // Only store in the HashMap if Optional.empty() wasn't returned
+            // (meaning an error occurred during initialization).
+            this.getPhraseClip(phrase).ifPresent(clip -> phraseToClip.put(phrase, clip));
+        }
+        return phraseToClip;
     }
 
     /**
@@ -58,8 +92,13 @@ public class AudioPlayer implements Runnable {
 
         // Resets the audio stream to the first remaining Phrase & start the clip.
         synchronized (this) {
-            this.resetAudioStream(this.phrasesToPlay.remove(0).getPhraseAudioFile());
-            this.clip.start();
+            Phrase phraseToPlay = this.phrasesToPlay.remove(0);
+            Clip clipToPlay = this.phraseToClip.get(phraseToPlay);
+            if (clipToPlay != null) {
+                this.activeClip = clipToPlay;
+                clipToPlay.setMicrosecondPosition(0);
+                clipToPlay.start();
+            }
         }
     }
 
@@ -69,9 +108,9 @@ public class AudioPlayer implements Runnable {
      * @param phrase The {@link Phrase} to replace the phrases to play with.
      */
     public void replacePhraseToPlay(Phrase phrase) {
-        // Since I'm REPLACING the phrases to play, I need to first close the running clip.
-        if (this.clip.isRunning()) {
-            this.clip.close();
+        // Since I'm REPLACING the phrases to play, I need to first stop the running clip.
+        if (this.activeClip.isRunning()) {
+            this.activeClip.stop();
         }
 
         synchronized (this) {
@@ -85,12 +124,12 @@ public class AudioPlayer implements Runnable {
      * @param phrases The {@link ArrayList} of {@link Phrase}s to replace the phrases to play with.
      */
     public void replacePhraseToPlay(ArrayList<Phrase> phrases) {
-        synchronized (this) {
-            // Since I'm REPLACING the phrases to play, I need to first close the running clip.
-            if (this.clip.isRunning()) {
-                this.clip.close();
-            }
+        // Since I'm REPLACING the phrases to play, I need to first stop the running clip.
+        if (this.activeClip.isRunning()) {
+            this.activeClip.stop();
+        }
 
+        synchronized (this) {
             this.phrasesToPlay = phrases;
         }
     }
@@ -100,22 +139,19 @@ public class AudioPlayer implements Runnable {
      */
     @Override
     public void run() {
-        // Terminate only when isActive is set to false.
+        // Terminate the thread only when isActive is set to false.
         while (this.isActive) {
-            if (!this.clip.isRunning()) {
-                long start = System.currentTimeMillis();
-                this.clip.close();
-                long end = System.currentTimeMillis();
-                System.out.println("duration: " + (end - start));
+            if (!this.activeClip.isRunning()) {
+                this.activeClip.stop();
 
                 synchronized (this) {
                     this.playPhrase();
                 }
             }
 
+            // Sleep momentarily to prevent the audio player from skipping over phrases.
             try {
-                // Divide by 1000 to convert microseconds to milliseconds.
-                Thread.sleep(1);
+                Thread.sleep(10);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -126,8 +162,6 @@ public class AudioPlayer implements Runnable {
      * Terminate the audio player (sets the isActive flag to false).
      */
     public void terminateAudioPlayer() {
-        synchronized (this) {
-            this.isActive = false;
-        }
+        this.isActive = false;
     }
 }
