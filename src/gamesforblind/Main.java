854c8f8d20507c20d44de476e4a78093c378d5cd
Alex Hadi <alexhisb@gmail.com>
2019-11-16 10:16:03 -0800

    Increase Sudoku menu font size.

--- src/gamesforblind/Main.java
+++ src/gamesforblind/Main.java
@@ -3,18 +3,33 @@ package gamesforblind;
 import gamesforblind.loader.GameLoader;
 import gamesforblind.synthesizer.AudioFileBuilder;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Class contains the sole entry point for the program.
  */
 public class Main {
+    /**
+     * Sets the GUI attributes that are used throughout the games (e.g. increasing menu font size.
+     */
+    private static void setUIAttributes() {
+        // Increase the font size for the game menus & make it bold.
+        Font MENU_FONT = new Font("Arial", Font.BOLD, 36);
+        UIManager.put("Menu.font", MENU_FONT);
+        UIManager.put("MenuItem.font", MENU_FONT);
+
+        // Allows "COMMAND" + "Q" to trigger the XML log save.
+        System.setProperty("apple.eawt.quitStrategy", "CLOSE_ALL_WINDOWS");
+    }
+
     /**
      * The sole entry point into the program.
      *
      * @param args The command line arguments to the program.
      */
     public static void main(String[] args) {
-        // Allows "COMMAND" + "Q" to trigger the XML log save.
-        System.setProperty("apple.eawt.quitStrategy", "CLOSE_ALL_WINDOWS");
+        setUIAttributes();
 
         if (Constants.BUILD_PHRASES) {
             AudioFileBuilder audioFileBuilder = new AudioFileBuilder();
