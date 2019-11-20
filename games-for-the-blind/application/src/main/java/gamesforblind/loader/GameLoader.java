ff2876131cbe7f7dbadce3fd746fad23535191df
Alex Hadi <alexhisb@gmail.com>
2019-11-20 13:13:29 -0800

    Replaced chained instanceof checks with Maps.

--- games-for-the-blind/application/src/main/java/gamesforblind/loader/GameLoader.java
+++ games-for-the-blind/application/src/main/java/gamesforblind/loader/GameLoader.java
@@ -142,104 +142,109 @@ public class GameLoader {
      * Receives an action that was sent by either {@link LoaderActionListener} or {@link LoaderKeyboardListener}.
      * Modifies the GUI appropriately based on the given action that was sent.
      *
-     * @param action The action that was sent to the game loader.
+     * @param loaderAction The action that was sent to the game loader.
      */
-    public void receiveAction(LoaderAction action) {
+    public void receiveAction(LoaderAction loaderAction) {
         if (!this.programArgs.isPlaybackMode()) {
-            this.logFactory.addProgramAction(action);
+            this.logFactory.addProgramAction(loaderAction);
         }
 
-        // Case 1: the user pressed one of the arrow keys in the loader GUI.
-        if (action instanceof LoaderArrowKeyAction) {
-            final Map<String, Phrase> BUTTON_TEXT_TO_PHRASE = Map.of(
-                    PLAY_SUDOKU_BUTTON, Phrase.SPACE_FOR_SUDOKU,
-                    EXIT_BUTTON, Phrase.SPACE_FOR_EXIT,
-                    BACK_BUTTON, Phrase.GO_BACK_TO_GAME_SELECTION,
-                    FOUR_BY_FOUR_SUDOKU_BUTTON, Phrase.SELECT_SUDOKU_FOUR,
-                    SIX_BY_SIX_SUDOKU_BUTTON, Phrase.SELECT_SUDOKU_SIX,
-                    NINE_BY_NINE_SUDOKU_BUTTON, Phrase.SELECT_SUDOKU_NINE
-            );
+        final Map<Class, Runnable> LOADER_ACTION_TO_RUNNABLE = Map.of(
+                // Case 1: the user pressed one of the arrow keys in the loader GUI.
+                LoaderArrowKeyAction.class, () -> {
+                    final Map<String, Phrase> BUTTON_TEXT_TO_PHRASE = Map.of(
+                            PLAY_SUDOKU_BUTTON, Phrase.SPACE_FOR_SUDOKU,
+                            EXIT_BUTTON, Phrase.SPACE_FOR_EXIT,
+                            BACK_BUTTON, Phrase.GO_BACK_TO_GAME_SELECTION,
+                            FOUR_BY_FOUR_SUDOKU_BUTTON, Phrase.SELECT_SUDOKU_FOUR,
+                            SIX_BY_SIX_SUDOKU_BUTTON, Phrase.SELECT_SUDOKU_SIX,
+                            NINE_BY_NINE_SUDOKU_BUTTON, Phrase.SELECT_SUDOKU_NINE
+                    );
 
-            LoaderArrowKeyAction loaderArrowKeyAction = (LoaderArrowKeyAction) action;
-            this.loaderFrame.changeHighlightedButton(loaderArrowKeyAction.getArrowKeyDirection());
-            this.audioPlayerExecutor.replacePhraseAndPrint(
-                    BUTTON_TEXT_TO_PHRASE.get(this.loaderFrame.getCurrentlyHighlightedButtonText())
-            );
-            return;
-        }
+                    LoaderArrowKeyAction loaderArrowKeyAction = (LoaderArrowKeyAction) loaderAction;
+                    this.loaderFrame.changeHighlightedButton(loaderArrowKeyAction.getArrowKeyDirection());
+                    this.audioPlayerExecutor.replacePhraseAndPrint(
+                            BUTTON_TEXT_TO_PHRASE.get(this.loaderFrame.getCurrentlyHighlightedButtonText())
+                    );
+                },
 
-        // Case 2: the user changed the currently selected game in the loader GUI.
-        if (action instanceof LoaderGameSelectionAction) {
-            LoaderGameSelectionAction gameAction = (LoaderGameSelectionAction) action;
+                // Case 2: the user changed the currently selected game in the loader GUI.
+                LoaderGameSelectionAction.class, () -> {
+                    LoaderGameSelectionAction gameAction = (LoaderGameSelectionAction) loaderAction;
 
-            Phrase relevantPhrase;
-            if (gameAction.getSelectedGame() == SelectedGame.SUDOKU) {
-                if (this.programArgs.getSelectedInterfaceType() == InterfaceType.ARROW_KEY_INTERFACE) {
-                    relevantPhrase = Phrase.WHICH_SUDOKU_GAME_ALL;
-                } else {
-                    relevantPhrase = Phrase.WHICH_SUDOKU_GAME_NO_SIX;
+                    Phrase relevantPhrase;
+                    if (gameAction.getSelectedGame() == SelectedGame.SUDOKU) {
+                        if (this.programArgs.getSelectedInterfaceType() == InterfaceType.ARROW_KEY_INTERFACE) {
+                            relevantPhrase = Phrase.WHICH_SUDOKU_GAME_ALL;
+                        } else {
+                            relevantPhrase = Phrase.WHICH_SUDOKU_GAME_NO_SIX;
+                        }
+                    } else {
+                        relevantPhrase = Phrase.PLAY_OR_EXIT;
+                    }
+                    this.audioPlayerExecutor.replacePhraseAndPrint(relevantPhrase);
+
+                    this.loaderFrame.setLoaderGuiBasedOnSelectedGame(gameAction.getSelectedGame());
+                },
+
+                // Case 3: the user selected one of the Sudoku board sizes in the loader GUI (4x4 or 9x9).
+                LoaderSudokuSelectionAction.class, () -> {
+                    LoaderSudokuSelectionAction loaderSudokuSelection = (LoaderSudokuSelectionAction) loaderAction;
+                    SudokuType sudokuType = loaderSudokuSelection.getSudokuType();
+
+                    ArrayList<InterfaceType> supportedInterfaces = sudokuType.getSupportedSudokuInterfaces();
+                    InterfaceType interfaceType = this.programArgs.getSelectedInterfaceType();
+                    if (!supportedInterfaces.contains(interfaceType)) {
+                        System.err.println(
+                                "Interface type " + interfaceType + " not supported for Sudoku type " + sudokuType
+                        );
+                        System.exit(1);
+                    }
+
+                    this.loaderFrame.closeLoaderFrames();
+                    this.sudokuGame = new SudokuGame(
+                            this, sudokuType, this.audioPlayerExecutor, this.logFactory, this.programArgs
+                    );
+                },
+
+                // Case 4: the user pressed an unrecognized key on the keyboard.
+                LoaderUnrecognizedKeyAction.class, () -> {
+                    LoaderUnrecognizedKeyAction unrecognizedKeyAction = (LoaderUnrecognizedKeyAction) loaderAction;
+                    this.audioPlayerExecutor.replacePhraseAndPrint(new ArrayList<>(Arrays.asList(
+                            Phrase.UNRECOGNIZED_KEY, Phrase.keyCodeToPhrase(unrecognizedKeyAction.getKeyCode())
+                    )));
+                },
+
+                // Case 5: the user has decided to exit the loader GUI.
+                LoaderExitAction.class, () -> {
+                    this.audioPlayerExecutor.replacePhraseAndPrint(Phrase.EXITING);
+
+                    // I don't want to exit the game if it is in playback mode (see the ending state of the game).
+                    if (this.programArgs.isPlaybackMode()) {
+                        return;
+                    }
+
+                    this.audioPlayerExecutor.terminateAudioPlayer();
+
+                    LogWriter logWriter = new LogWriter(this.logFactory);
+                    logWriter.saveGameLog();
+
+                    // Wait for the audio player thread to end.
+                    try {
+                        this.audioPlayerThread.join();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    System.exit(0);
                 }
-            } else {
-                relevantPhrase = Phrase.PLAY_OR_EXIT;
-            }
-            this.audioPlayerExecutor.replacePhraseAndPrint(relevantPhrase);
+        );
 
-            this.loaderFrame.setLoaderGuiBasedOnSelectedGame(gameAction.getSelectedGame());
-            return;
-        }
-
-        // Case 3: the user selected one of the Sudoku board sizes in the loader GUI (4x4 or 9x9).
-        if (action instanceof LoaderSudokuSelectionAction) {
-            LoaderSudokuSelectionAction loaderSudokuSelectionAction = (LoaderSudokuSelectionAction) action;
-            SudokuType sudokuType = loaderSudokuSelectionAction.getSudokuType();
-
-            ArrayList<InterfaceType> supportedInterfaces = sudokuType.getSupportedSudokuInterfaces();
-            InterfaceType selectedInterfaceType = this.programArgs.getSelectedInterfaceType();
-            if (!supportedInterfaces.contains(selectedInterfaceType)) {
-                System.err.println(
-                        "Interface type " + selectedInterfaceType + " not supported for Sudoku type " + sudokuType
-                );
-                System.exit(1);
-            }
-
-            this.loaderFrame.closeLoaderFrames();
-            this.sudokuGame = new SudokuGame(
-                    this, sudokuType, this.audioPlayerExecutor, this.logFactory, this.programArgs
-            );
-            return;
-        }
-
-        // Case 4: the user pressed an unrecognized key on the keyboard.
-        if (action instanceof LoaderUnrecognizedKeyAction) {
-            LoaderUnrecognizedKeyAction loaderUnrecognizedKeyAction = (LoaderUnrecognizedKeyAction) action;
-            this.audioPlayerExecutor.replacePhraseAndPrint(new ArrayList<>(Arrays.asList(
-                    Phrase.UNRECOGNIZED_KEY, Phrase.keyCodeToPhrase(loaderUnrecognizedKeyAction.getKeyCode())
-            )));
-            return;
-        }
-
-        // Case 5: the user has decided to exit the loader GUI.
-        if (action instanceof LoaderExitAction) {
-            this.audioPlayerExecutor.replacePhraseAndPrint(Phrase.EXITING);
-
-            // I don't want to actually exit the game if it is in playback mode (see the ending state of the game).
-            if (this.programArgs.isPlaybackMode()) {
-                return;
-            }
-
-            this.audioPlayerExecutor.terminateAudioPlayer();
-
-            LogWriter logWriter = new LogWriter(this.logFactory);
-            logWriter.saveGameLog();
-
-            // Wait for the audio player thread to end.
-            try {
-                this.audioPlayerThread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            System.exit(0);
+        Runnable functionToExecute = LOADER_ACTION_TO_RUNNABLE.get(loaderAction.getClass());
+        if (functionToExecute != null) {
+            functionToExecute.run();
+        } else {
+            System.err.println("An unrecognized form of loader action was received!");
         }
     }
 
--- games-for-the-blind/application/src/main/java/gamesforblind/loader/action/LoaderExitAction.java
+++ games-for-the-blind/application/src/main/java/gamesforblind/loader/action/LoaderExitAction.java
@@ -5,8 +5,7 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
- * Action for when the user wishes to exit from the loader screen. The body of this class is empty
- * because the method to receive the action just needs to check for "instanceof" the passed action.
+ * Action for when the user wishes to exit from the loader screen.
  */
 @XmlRootElement(name = "LoaderExitAction")
 @XmlAccessorType(XmlAccessType.NONE)
