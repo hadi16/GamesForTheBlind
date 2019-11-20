d7d1798de5f2241f0ffc0910c351b269b1269ebd
Alex Hadi <alexhisb@gmail.com>
2019-11-20 13:13:29 -0800

    Replaced chained instanceof checks with Maps.

--- games-for-the-blind/application/src/main/java/gamesforblind/sudoku/SudokuGame.java
+++ games-for-the-blind/application/src/main/java/gamesforblind/sudoku/SudokuGame.java
@@ -10,6 +10,8 @@ import gamesforblind.sudoku.action.*;
 import gamesforblind.sudoku.gui.SudokuFrame;
 import gamesforblind.synthesizer.AudioPlayerExecutor;
 
+import java.util.Map;
+
 /**
  * Game class that is called directly from the {@link GameLoader} class.
  * It receives actions & sends these actions to the {@link SudokuState} for the game.
@@ -65,12 +67,6 @@ public class SudokuGame {
             this.logFactory.addProgramAction(sudokuAction);
         }
 
-        // Case 1: the user wishes to exit the game.
-        if (sudokuAction instanceof SudokuExitAction) {
-            this.gameLoader.receiveAction(new LoaderExitAction());
-            return;
-        }
-
         // If the game is over, go to main menu
         if (this.sudokuState.isGameOver()) {
             this.sudokuFrame.closeFrames();
@@ -78,81 +74,81 @@ public class SudokuGame {
             return;
         }
 
-        // Case 2: the user wants the instructions for the game to be read.
-        if (sudokuAction instanceof SudokuInstructionsAction) {
-            this.sudokuState.readInstructions();
-            return;
+        final Map<Class, Runnable> SUDOKU_ACTION_TO_RUNNABLE = Map.of(
+                // Case 1: the user wishes to exit the game.
+                SudokuExitAction.class, () -> this.gameLoader.receiveAction(new LoaderExitAction()),
+
+                // Case 2: the user wants the instructions for the game to be read.
+                SudokuInstructionsAction.class, this.sudokuState::readInstructions,
+
+                // Case 3: the user wants a hint.
+                SudokuHintKeyAction.class, () -> {
+                    this.sudokuState.giveHint();
+                    this.sudokuFrame.repaintSudokuPanel();
+                },
+
+                // Case 4: the user has pressed an unrecognized key on the keyboard.
+                SudokuUnrecognizedKeyAction.class, () -> {
+                    SudokuUnrecognizedKeyAction unrecognizedKeyAction = (SudokuUnrecognizedKeyAction) sudokuAction;
+                    this.sudokuState.readUnrecognizedKey(unrecognizedKeyAction.getKeyCode());
+                },
+
+                // Case 5: the user hits a hot key (e.g. Ctrl + LEFT)
+                SudokuHotKeyAction.class, () -> {
+                    SudokuHotKeyAction sudokuHotKeyAction = (SudokuHotKeyAction) sudokuAction;
+                    this.sudokuState.setHighlightedPoint(sudokuHotKeyAction.getArrowKeyDirection());
+                    this.sudokuState.readSelectedSquare();
+                    this.sudokuFrame.repaintSudokuPanel();
+                },
+
+                // Case 6: the user wants to highlight a square/block on the board.
+                SudokuHighlightAction.class, () -> {
+                    SudokuHighlightAction highlightAction = (SudokuHighlightAction) sudokuAction;
+                    this.sudokuState.setHighlightedPoint(
+                            highlightAction.getPointToHighlight(), highlightAction.getInputType()
+                    );
+                    this.sudokuState.readSelectedSquare();
+                    this.sudokuFrame.repaintSudokuPanel();
+                },
+
+                // Case 7: the user wants to fill a square on the board.
+                SudokuFillAction.class, () -> {
+                    SudokuFillAction sudokuFillAction = (SudokuFillAction) sudokuAction;
+                    this.sudokuState.setSquareNumber(sudokuFillAction.getNumberToFill());
+                    this.sudokuFrame.repaintSudokuPanel();
+                },
+
+                // Case 8: the user wants the current row/column/block to be read.
+                SudokuReadPositionAction.class, () -> {
+                    SudokuReadPositionAction sudokuReadPositionAction = (SudokuReadPositionAction) sudokuAction;
+                    this.sudokuState.readBoardSection(sudokuReadPositionAction.getSudokuSection());
+                    this.sudokuFrame.repaintSudokuPanel();
+                },
+
+                // Case 9: return to main menu.
+                SudokuMainMenuAction.class, () -> {
+                    this.sudokuFrame.closeFrames();
+                    this.gameLoader.openLoaderInterface();
+                },
+
+                // Case 10: restart the current Sudoku board.
+                SudokuRestartAction.class, () -> {
+                    if (this.programArgs.isPlaybackMode()) {
+                        this.sudokuState.resetSudokuState(this.logFactory.popOriginalGridFromFront());
+                    } else {
+                        this.sudokuState.resetSudokuState(null);
+                        this.logFactory.addOriginalSudokuGrid(this.sudokuState.getOriginalGrid());
+                    }
+
+                    this.sudokuFrame.repaintSudokuPanel();
+                }
+        );
+
+        Runnable functionToExecute = SUDOKU_ACTION_TO_RUNNABLE.get(sudokuAction.getClass());
+        if (functionToExecute != null) {
+            functionToExecute.run();
+        } else {
+            System.err.println("An unrecognized form of a Sudoku action was received by the game!");
         }
-
-        // Case 3: the user wants a hint.
-        if (sudokuAction instanceof SudokuHintKeyAction) {
-            this.sudokuState.giveHint();
-            this.sudokuFrame.repaintSudokuPanel();
-            return;
-        }
-
-        // Case 4: the user has pressed an unrecognized key on the keyboard.
-        if (sudokuAction instanceof SudokuUnrecognizedKeyAction) {
-            SudokuUnrecognizedKeyAction sudokuUnrecognizedKeyAction = (SudokuUnrecognizedKeyAction) sudokuAction;
-            this.sudokuState.readUnrecognizedKey(sudokuUnrecognizedKeyAction.getKeyCode());
-            return;
-        }
-
-        // Case 5: the user hits a hot key (e.g. Ctrl + LEFT)
-        if (sudokuAction instanceof SudokuHotKeyAction) {
-            SudokuHotKeyAction sudokuHotKeyAction = (SudokuHotKeyAction) sudokuAction;
-            this.sudokuState.setHighlightedPoint(sudokuHotKeyAction.getArrowKeyDirection());
-            this.sudokuState.readSelectedSquare();
-            this.sudokuFrame.repaintSudokuPanel();
-            return;
-        }
-
-        // Case 6: the user wants to highlight a square/block on the board.
-        if (sudokuAction instanceof SudokuHighlightAction) {
-            SudokuHighlightAction highlightAction = (SudokuHighlightAction) sudokuAction;
-            this.sudokuState.setHighlightedPoint(highlightAction.getPointToHighlight(), highlightAction.getInputType());
-            this.sudokuState.readSelectedSquare();
-            this.sudokuFrame.repaintSudokuPanel();
-            return;
-        }
-
-        // Case 7: the user wants to fill a square on the board.
-        if (sudokuAction instanceof SudokuFillAction) {
-            SudokuFillAction sudokuFillAction = (SudokuFillAction) sudokuAction;
-            this.sudokuState.setSquareNumber(sudokuFillAction.getNumberToFill());
-            this.sudokuFrame.repaintSudokuPanel();
-            return;
-        }
-
-        // Case 8: the user wants the current row/column/block to be read.
-        if (sudokuAction instanceof SudokuReadPositionAction) {
-            SudokuReadPositionAction sudokuReadPositionAction = (SudokuReadPositionAction) sudokuAction;
-            this.sudokuState.readBoardSection(sudokuReadPositionAction.getSudokuSection());
-            this.sudokuFrame.repaintSudokuPanel();
-            return;
-        }
-
-        // Case 9: return to main menu.
-        if (sudokuAction instanceof SudokuMainMenuAction) {
-            this.sudokuFrame.closeFrames();
-            this.gameLoader.openLoaderInterface();
-            return;
-        }
-
-        // Case 10: restart the current Sudoku board.
-        if (sudokuAction instanceof SudokuRestartAction) {
-            if (this.programArgs.isPlaybackMode()) {
-                this.sudokuState.resetSudokuState(this.logFactory.popOriginalGridFromFront());
-            } else {
-                this.sudokuState.resetSudokuState(null);
-                this.logFactory.addOriginalSudokuGrid(this.sudokuState.getOriginalGrid());
-            }
-
-            this.sudokuFrame.repaintSudokuPanel();
-            return;
-        }
-
-        // Case 11: error
-        System.err.println("An unrecognized form of a Sudoku action was received by the game!");
     }
 }
